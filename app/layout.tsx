16eab4c64d7050820a955e3644b931f2250eb7f2
wisecoder17 <richardogunwole17@gmail.com>
2025-07-06 06:40:00 +0100

    Finalise font's locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { Poppins } from 'next/font/google' // review font bug
-
 import "./globals.css";
 
-const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-    variable: '--font-poppins',
-})
 
 export const metadata: Metadata = {
   title: "StoreIt",
@@ -17,13 +10,12 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
+  // eslint-disable-next-line no-undef
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
+      <body className="font-poppins antialiased">
         {children}
       </body>
     </html>
